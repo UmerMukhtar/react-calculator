6483677cfb10fedb0a192d868c92f0dc94f2400a
Umer Mukhtar <xsumer69@gmail.com>
2022-09-04 16:30:27 +0500

    Completed UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 
-import './App.css';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-  
+    <div className="Calculator-grid">
+    <div className='output'>
+      <div className='previous-operand'>123,234</div>
+      <div className='current-operand'>123,234</div>
    </div>
-  );
-}
+   <button className='span-two'>AC</button>
+   <button>Del</button>
+   <button>÷</button>
+   <button>1</button>
+   <button>2</button>
+   <button>3</button>
+   <button>+</button>
+   <button>4</button>
+   <button>5</button>
+   <button>6</button>
+   <button>+</button>
+   <button>7</button>
+   <button>8</button>
+   <button>9</button>
+   <button>-</button>
+   <button>.</button>
+   <button>0</button>
+    <button className='span-two'>=</button>
+  </div>
+  )
+};
+
+
 
 export default App;
